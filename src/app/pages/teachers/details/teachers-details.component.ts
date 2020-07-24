d047e59f69784a83cc48c72f3500b8d75bbed521
Daniel <dnuneswebdev@gmail.com>
2020-07-24 11:45:53 -0300

    Created students, teachers, calendar, system-settings pages components and folder structure

---
+++ src/app/pages/teachers/details/teachers-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teachers-details',
+  templateUrl: './teachers-details.component.html',
+  styleUrls: ['./teachers-details.component.css']
+})
+export class TeachersDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
