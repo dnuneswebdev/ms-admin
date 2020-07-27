84a8e2c81f25c2a4b78c0fab2a0a43299860eb2d
Daniel <dnuneswebdev@gmail.com>
2020-07-27 12:23:52 -0300

    Added icon-button on toolbar with profile and logout dropdown menu; minor css ajudstemnts

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -1,10 +1,15 @@
+//Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Material
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { HeaderComponent } from './header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+//Components
+import { HeaderComponent } from './header.component';
 
 
 @NgModule({
@@ -15,7 +20,8 @@ import { MatIconModule } from '@angular/material/icon';
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule,
   ],
   exports: [
     HeaderComponent
