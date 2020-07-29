afee9c95598464be88113a318d61e1ff01ebec3d
Daniel <dnuneswebdev@gmail.com>
2020-07-29 15:00:45 -0300

    Added datatable filter, sort, paginator and loading spinner;

--- src/app/shared/components/datatable/datatable.component.ts
+++ src/app/shared/components/datatable/datatable.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-datatable',
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.css']
 })
-export class DatatableComponent implements OnInit {
+export class DatatableComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   @Input() tableData: Array<any>;
   @Input() tableColumns: Array<string>;
   @Input() tableHeaderCells: Array<string>;
 
+  isLoading: boolean = true;
+  matTableDataSource: MatTableDataSource<any>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  ngOnChanges() {
+    if (this.tableData !== undefined) {
+      this.matTableDataSource = new MatTableDataSource(this.tableData);
+      this.matTableDataSource.sort = this.sort;
+      this.matTableDataSource.paginator = this.paginator;
+      this.isLoading = false;
+    }
+  }
+
+  filterTable(value: string) {
+    this.matTableDataSource.filter = value.trim().toLowerCase();
   }
 
 }
--- src/app/shared/components/datatable/datatable.module.ts
+++ src/app/shared/components/datatable/datatable.module.ts
@@ -10,6 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 //Component
 import { DatatableComponent } from './datatable.component';
@@ -29,6 +30,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatButtonModule,
     MatFormFieldModule,
     FlexLayoutModule,
+    MatProgressSpinnerModule,
   ],
   exports: [
     DatatableComponent
