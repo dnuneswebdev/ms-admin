c0e2e53a2008fb659b8ede5537c8a6fc38d24d00
Daniel <dnuneswebdev@gmail.com>
2020-08-03 16:03:36 -0300

    #Teachers CRUD - Created all http services for Teachers CRUD; Added new roles array at db.json

--- src/app/pages/teachers/services/teachers.service.ts
+++ src/app/pages/teachers/services/teachers.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Teacher } from '../models/teacher.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,25 @@ export class TeachersService {
 
   getAllTeachers() {
     return this.http.get(`${environment.api}/teachers`);
+  }
+
+  getTeacher(id: number) {
+    return this.http.get(`${environment.api}/teachers/${id}`);
+  }
+
+  createNewTeacher(teacher: Teacher) {
+    return this.http.post(`${environment.api}/teachers`, teacher);
+  }
+
+  updateTeacher(id: number, teacher: Teacher) {
+    return this.http.put(`${environment.api}/teachers/${id}`, teacher);
+  }
+
+  deleteTeacher(id: number) {
+    return this.http.delete(`${environment.api}/teachers/${id}`);
+  }
+
+  getAllRoles() {
+    return this.http.get(`${environment.api}/roles`);
   }
 }
